df28aa1570c00faaa1613def97a034ec23f6ace4
Anne-Marie Rommerdahl <annro@itu.dk>
2025-04-23 11:46:19 +0200

    reduced cognitice complexity in registerhandler

--- minitwit/handlers/register.go
+++ minitwit/handlers/register.go
@@ -16,6 +16,48 @@ import (
 
 var registerTmpl = template.Must(template.ParseFiles("templates/layout.html", "templates/register.html"))
 
+func register_user(w http.ResponseWriter, r *http.Request, database *gorm.DB) {
+	username := r.FormValue("username")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	password2 := r.FormValue("password2")
+
+	// input validation
+	if username == "" || email == "" || password == "" {
+		http.Error(w, "You must fill out all fields", http.StatusBadRequest)
+		return
+	}
+
+	// Check if repeated password matches
+	if password != password2 {
+		http.Error(w, "Passwords do not match", http.StatusBadRequest)
+		return
+	}
+
+	_, err := db.GormGetUserId(database, username)
+	if err == nil {
+		http.Error(w, "User already exists", http.StatusBadRequest)
+		return
+	}
+
+	// hash the password
+	hash := md5.New()
+	hash.Write([]byte(password))
+	pwHash := hex.EncodeToString(hash.Sum(nil))
+
+	// insert the user into the database
+	user := models.User{Username: username, Email: email, PwHash: pwHash}
+	result := database.Create(&user)
+	if result.Error != nil {
+		log.Fatalf("Failed to insert in db: %v", err)
+		return
+	}
+
+	// redirect to timeline
+	utils.AddFlash(w, r, "You were successfully registered and can login now")
+	http.Redirect(w, r, "/login", http.StatusFound)
+}
+
 func RegisterHandler(database *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
@@ -24,45 +66,7 @@ func RegisterHandler(database *gorm.DB) http.HandlerFunc {
 			}
 		}
 		if r.Method == "POST" {
-			username := r.FormValue("username")
-			email := r.FormValue("email")
-			password := r.FormValue("password")
-			password2 := r.FormValue("password2")
-
-			// input validation
-			if username == "" || email == "" || password == "" {
-				http.Error(w, "You must fill out all fields", http.StatusBadRequest)
-				return
-			}
-
-			// Check if repeated password matches
-			if password != password2 {
-				http.Error(w, "Passwords do not match", http.StatusBadRequest)
-				return
-			}
-
-			_, err := db.GormGetUserId(database, username)
-			if err == nil {
-				http.Error(w, "User already exists", http.StatusBadRequest)
-				return
-			}
-
-			// hash the password
-			hash := md5.New()
-			hash.Write([]byte(password))
-			pwHash := hex.EncodeToString(hash.Sum(nil))
-
-			// insert the user into the database
-			user := models.User{Username: username, Email: email, PwHash: pwHash}
-			result := database.Create(&user)
-			if result.Error != nil {
-				log.Fatalf("Failed to insert in db: %v", err)
-				return
-			}
-
-			// redirect to timeline
-			utils.AddFlash(w, r, "You were successfully registered and can login now")
-			http.Redirect(w, r, "/login", http.StatusFound)
+			register_user(w, r, database)
 		}
 	}
 }
